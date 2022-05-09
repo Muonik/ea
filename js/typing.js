39c4d0cdf0a1fdfeded51560e57c20ffbdbd8005
Muonik <muon1996@gmail.com>
2022-05-10 01:05:34 +0800

    updated animations etc

--- js/typing.js
+++ js/typing.js
@@ -1,8 +1,7 @@
 let iChar = 0;
-let textEl = document.getElementById("bg");
+let textEl = document.getElementById("typingg");
 let delay = 8; 
-let content = "i burrow into your phosphorus body like an earthworm, my lungs curl up in ecstasy i burrow into your phosphorus body like an earthworm, my lungs curl up in ecstasy i burrow into your phosphorus body like an earthworm, my lungs curl up in ecstasy i burrow into your phosphorus body like an earthworm, my lungs curl up in ecstasy i burrow into your phosphorus body like an earthworm, my lungs curl up in ecstasy i burrow into your phosphorus body like an earthworm, my lungs curl up in ecstasy i burrow into your phosphorus body like an earthworm, my lungs curl up in ecstasy i burrow into your phosphorus body like an earthworm, my lungs curl up in ecstasy i burrow into your phosphorus body like an earthworm, my lungs curl up in ecstasy i burrow into your phosphorus body like an earthworm, my lungs curl up in ecstasy i burrow into your phosphorus body like an earthworm, my lungs curl up in ecstasy i burrow into your phosphorus body like an earthworm, my lungs curl up in ecstasy i burrow into your phosphorus body like an earthworm, my lungs curl up in ecstasy i burrow into your phosphorus body like an earthworm, my lungs curl up in ecstasy"
-
+let content = "it welcomes me with open arms, enveloping me in a white flame. all of it is transmitted to me through bone conduction, through cellular telepathy, through viral infection, through parasitic possession, through screams, through whispers."
 
 function typeChar() 
 {
