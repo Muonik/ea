c1d2a6f54372d3e3c812823370d2f8a4bf7495e4
Muonik <muon1996@gmail.com>
2022-05-10 14:20:02 +0800

    updated texts

--- js/typing.js
+++ js/typing.js
@@ -1,7 +1,7 @@
 let iChar = 0;
 let textEl = document.getElementById("typingg");
 let delay = 8; 
-let content = "it welcomes me with open arms, enveloping me in a white flame. all of it is transmitted to me through bone conduction, through cellular telepathy, through viral infection, through parasitic possession, through screams, through whispers."
+let content = "it welcomes me with open arms, enveloping me in a white flame. all of it is transmitted to me through bone conduction, through cellular telepathy, through viral protocols, through parasitic possession, through screams, through whispers."
 
 function typeChar() 
 {
